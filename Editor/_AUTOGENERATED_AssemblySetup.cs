978ff2315f46e99bf6f0bef06f9bf1060b0a0310
Trond Glomnes <38559434+trondtactile@users.noreply.github.com>
2024-05-07 15:00:34 +0200

    editor assemlies now expose internals to both types of test assemblies

--- Editor/_AUTOGENERATED_AssemblySetup.cs
+++ Editor/_AUTOGENERATED_AssemblySetup.cs
@@ -1,4 +1,5 @@
 
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("com.tactilegames.PACKAGE_NAME.tests")]
+[assembly: InternalsVisibleTo("com.tactilegames.PACKAGE_NAME.editor.tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
