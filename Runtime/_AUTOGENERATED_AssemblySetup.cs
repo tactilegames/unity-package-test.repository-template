f4b3e871af20bf772561f4bc1fcf867e9cf8c09f
Trond Glomnes <38559434+trondtactile@users.noreply.github.com>
2024-05-07 15:04:24 +0200

    runtime assembly exposes internals to editor assembly

--- Runtime/_AUTOGENERATED_AssemblySetup.cs
+++ Runtime/_AUTOGENERATED_AssemblySetup.cs
@@ -1,5 +1,6 @@
 
 using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("com.tactilegames.PACKAGE_NAME.editor")]
 [assembly: InternalsVisibleTo("com.tactilegames.PACKAGE_NAME.editor.tests")]
 [assembly: InternalsVisibleTo("com.tactilegames.PACKAGE_NAME.tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
